70fa0da933128b05e0b3264dc8601d6f536dfb8c
Faith Mumbua <faithmumbua2010@gmail.com>
2026-07-26 09:43:00 +0300

    fix:fixed the toggle button

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ const menuCloseButton= document.getElementById("menu-close-button");
 
 menuOpenButton.addEventListener("click", ()=>{
     //Toggle mobile menu visibility
-    document.body.classList.toggle("show-mobile-menu");
+    document.body.classList.add("show-mobile-menu");
 
 })
 
 //Close menu when the close button is clicked
-menuCloseButton.addEventListener("click",()=> menuOpenButton.click());
+menuCloseButton.addEventListener("click",()=> {
+    document.body.classList.remove("show-mobile-menu")
+});
